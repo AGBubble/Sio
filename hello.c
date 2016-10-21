1a6abc4baa2292c0f1385453a079c2be270159fb
Aina <aina.andriana@gmail.com>
2016-10-21 15:26:40 +0200

    premier cours de dév

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include<stdio.h> // Librairie comprenant, entre autres la fonction printf
+
+main() //Fonction principale = point d'entrée du programme
+{
+    int i = 10;
+    int j = 5;
+
+        printf("La valeur de i : %d\n La valeur de j :%d", i,j); // %d = un entier
+
+
+
+}
+
