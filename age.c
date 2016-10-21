8b0fa16850d83f490cf313b904a0bac3b4be031b
Aina <aina.andriana@gmail.com>
2016-10-21 15:59:23 +0200

    21/10/2016 développement

--- age.c
+++ age.c
@@ -5,6 +5,6 @@ main()
     int age;
 
     printf("Veuillez entrer votre age :");
-    scanf("%d", &age); // & = adresse de
-    printf("%d", age);
+    scanf("%d", &age); // & = adresse de, donc on enregistre la valeur de la saisie à l'adresse de la variable age
+    printf("\n \n Vous avez %d ans", age);
 }
--- hello.c
+++ hello.c
@@ -7,7 +7,11 @@ main() //Fonction principale = point d'entrée du programme
     float k = 3.5;
 
 
-        printf("%.2f \n", (float)i/j); // %d = un entier // //le .2 pour n'afficher aque deux nombres après la virgule //le (float) permet d'afficher un réel //
+        printf("%.2f \n", (float)i/j);
+        /* %d = un entier
+        le .2 pour n'afficher aque deux nombres après la virgule
+        le (float) permet d'afficher un réel
+        */
 
         printf("%.2f", (float)i/j);
 
