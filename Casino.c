dfbdfaf46f305295afdb5c58ed4adc75b4f3b88b
Aina <aina.andriana@gmail.com>
2016-10-27 14:56:02 +0200

    casino

---
+++ Casino.c
@@ -0,0 +1,17 @@
+main ()
+{
+    int age;
+
+        printf("Entrez votre age");
+        scanf("%d", &age);
+
+    if(age >=18)
+    {
+        printf("\nBienvenue", age);
+    }
+    else
+    {
+        printf("Trop jeune");
+    }
+
+}
