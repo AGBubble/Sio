e01871f2502cbe90f6b81604a13c1958dfd707f3
Aina <aina.andriana@gmail.com>
2016-10-27 15:37:02 +0200

    exo1

--- usb.c
+++ usb.c
@@ -11,12 +11,32 @@ main()
     {
         float p1;
         p1 = nombre*prix;
-        printf(" vous devez payer %f",p1);
+        printf(" vous devez payer %.2f euros",p1);
     }
     else
     {
         float p2;
         p2 = nombre*prix*0.80;
-        printf("Vous devez payer %f",p2);
+        printf("Vous devez payer %.2f euros",p2);
     }
 }
+
+/** Correction
+
+main()
+{
+    int nbCles;
+    float prixTotal, pu=4.9;
+
+    printf("Combien de cles voulez-vous ?");
+    scanf("%d", &nbCles);
+
+    prixTotal = nbCles * pu;
+
+    if(nbCles >= 10)
+    {
+        prixTotal = prixTotal * 0.8;
+    }
+    printf("\n\nVous me devez %.2f euros", prixTotal);
+}
+**/
