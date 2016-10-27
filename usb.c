0211bc0a41f1504ad5122cb068252378798486e8
Aina <aina.andriana@gmail.com>
2016-10-27 16:48:55 +0200

    ahaha

--- usb.c
+++ usb.c
@@ -13,11 +13,24 @@ main()
         p1 = nombre*prix;
         printf(" vous devez payer %.2f euros",p1);
     }
-    else
+    else if (nombre >=10 && nombre <=20)
     {
         float p2;
+        float rem = 0;
         p2 = nombre*prix*0.80;
+        rem = p2*0.2;
         printf("Vous devez payer %.2f euros",p2);
+        printf("\n\n\nvous avez %.2f euros de remise",rem);
+
+    }
+    else
+    {
+        float p3;
+        float rem1 = 0;
+        p3 = nombre*prix*0.70;
+        rem1 = p3*0.3;
+        printf("Vous devez payer %.2f euros",p3);
+        printf("\n\n\nvous avez %.2f euros de remise",rem1);
     }
 }
 
@@ -33,7 +46,12 @@ main()
 
     prixTotal = nbCles * pu;
 
-    if(nbCles >= 10)
+    if(nbCles >= 20)
+    {
+        prixTotal = prixTotal * 0.7;
+    }
+
+    else if(nbCles >= 10)
     {
         prixTotal = prixTotal * 0.8;
     }
