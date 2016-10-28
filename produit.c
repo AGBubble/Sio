9ba453b84dbd48961431f7994539b9dbcfb87acc
Aina <aina.andriana@gmail.com>
2016-10-28 17:33:17 +0200

    produit.c

--- produit.c
+++ produit.c
@@ -1,16 +1,18 @@
+/* Exercice 1 */
+
 int main()
 {
-    int nb1;
+    int nb1; /* On déclare des variables pour chaque nombre que l'on doit entrer*/
     int nb2;
 
     printf("Entrez deux nombres\n");
-    scanf("%d %d", &nb1, &nb2);
+    scanf("%d %d", &nb1, &nb2); /*  La fonction qui va nous permettre d'entrer des valeurs */
 
-    int pdt = nb1 * nb2;
+    int pdt = nb1 * nb2; /* La variable qui contiendra le produit */
 
     printf("%d", pdt);
 
-    /*
+    /* LE PROGRAMME AVEC DES REELS
     float nb1;
     float nb2;
 
