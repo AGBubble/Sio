d5a7071d5d86c46e5e3b0ec31b46e955ac73e24d
Aina <aina.andriana@gmail.com>
2016-10-28 17:30:11 +0200

    produit.c

---
+++ produit.c
@@ -0,0 +1,24 @@
+int main()
+{
+    int nb1;
+    int nb2;
+
+    printf("Entrez deux nombres\n");
+    scanf("%d %d", &nb1, &nb2);
+
+    int pdt = nb1 * nb2;
+
+    printf("%d", pdt);
+
+    /*
+    float nb1;
+    float nb2;
+
+    printf("Entrez deux nombres\n");
+    scanf("%f %f", &nb1, &nb2);
+
+    float pdt = nb1 * nb2;
+
+    printf("%.2f", pdt);
+    */
+}
