af0c12642ce366a6e257285c4bd4b751f6da0075
Aina <aina.andriana@gmail.com>
2016-11-03 16:13:03 +0100

    boucle

---
+++ boucle1.c
@@ -0,0 +1,45 @@
+/* int main ()
+{
+    int i = 0;
+
+    while (i < 10)
+    {
+        printf("\n%d", i);
+        i++;
+    }
+} */
+
+/*
+int main ()
+{
+    int i = 20;
+    while (i >= 10)
+    {
+        printf("\n%d", i);
+        i -= 2;
+    }
+}
+*/
+
+
+/*
+int main()
+
+{
+    int i;
+    i = 0;
+    while (i <= 81 )
+    {
+        if (i <= 40)
+            printf(" ");
+        else if (i == 41)
+            printf("*");
+        else
+            printf(" ");
+        i+= 1;
+    }
+
+*/
+
+
+}
