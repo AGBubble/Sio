feac4fbd1499250e2ce4ed36ec9a2c05b8d8e75e
Aina <aina@Ainas-MacBook-Pro.local>
2016-12-08 16:32:03 +0100

    pushexpli

---
+++ boucle.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	int i = 0;
+	
+	/*
+	
+	while (i < 10)
+	{
+		printf("Vatogba\n");
+		i += 2;
+		i++;
+		i = i + 1;
+		i += 1;
+		 
+	
+		
+		
+	}
+	*/
+	
+	
+	
+	
+}
---
+++ vraifaux.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	int result;
+		
+	printf("Combien font 2+2\n");
+	scanf("%d", &result);
+	
+	if (result == 4)
+	{
+		printf("VRAI");
+	}
+	else 
+		printf("FAUX");
+}
